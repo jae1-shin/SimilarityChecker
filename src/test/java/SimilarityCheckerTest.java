aafbb87d900a05eda2cb38677f294231746d67b6
jae1-shin <blessed9589@gmail.com>
2025-06-04 16:43:08 +0900

    [refactor]

--- src/test/java/SimilarityCheckerTest.java
+++ src/test/java/SimilarityCheckerTest.java
@@ -1,11 +1,9 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimilarityCheckerTest {
     private SimilarityChecker checker;
-
 
     @Test
     void createSimilarityChecker() {
